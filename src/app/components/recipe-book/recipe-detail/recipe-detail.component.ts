46a679288cac9db60c17a4fda903ab60ed948e72
Harry Frank <harry.frank@1901group.com>
2018-07-10 15:36:42 -0400

    Added base components for app

---
+++ src/app/components/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-recipe-detail',
+  styleUrls: [
+    './recipe-detail.component.css'
+  ],
+  templateUrl: './recipe-detail.component.html'
+})
+
+export class RecipeDetailComponent {
+
+}
