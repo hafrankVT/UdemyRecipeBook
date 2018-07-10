004af6d7665c50a03081a88437594a3369284a5f
Harry 'Alan' Frank, Jr <harry.frank@1901group.com>
2018-07-10 16:11:41 -0400

    Updated folder and component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './components/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeaderComponent } from './components/header/header.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
