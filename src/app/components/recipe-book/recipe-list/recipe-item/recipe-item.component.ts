5d20c34972d3517dd05e657f9da7f18d8388da7d
Harry Alan Frank Jr <harry.frank@1901group.com>
2018-07-17 15:51:55 -0400

    Implemented recipe-item

--- src/app/components/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  // Receive the recipe to be displayed
+  @Input() recipe: Recipe;
 
   constructor() { }
 
