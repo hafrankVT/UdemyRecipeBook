b832fde667c4765520a05cd4c37c02481311081c
Harry Alan Frank Jr <harry.frank@1901group.com>
2018-07-18 10:15:55 -0400

    Added ability to add ingredient to ingredients list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
@@ -19,10 +20,11 @@ import { RecipeBookComponent } from './components/recipe-book/recipe-book.compon
     RecipeItemComponent,
     RecipeDetailComponent,
     HeaderComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../../models/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInpuRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddIngredient(amount: HTMLInputElement, name: HTMLInputElement) {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmt = this.amountInpuRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmt);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
